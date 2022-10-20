cccb4a30f57784305476d5b2523d6ff31af38ed3
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2022-10-20 19:10:45 +0600

    Redux Adding

---
+++ src/redux/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  loading: false,
+  error: false,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginStart: (state) => {
+      state.loading = true;
+    },
+    loginSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+    },
+    loginFaild: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+    logout: (state) => {
+      return initialState;
+    },
+  },
+});
